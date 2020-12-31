76db617e282ef2faf07328715969b22c256fa475
Sue Cho <34840140+Sueaty@users.noreply.github.com>
2021-01-01 04:39:49 +0900

    Create 문자열내마음대로정렬하기.cpp

---
+++ Programmers/문자열내마음대로정렬하기.cpp
@@ -0,0 +1,25 @@
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int index;
+
+bool strangeOrder(string a, string b){ 
+    if(a[index] != b[index]){
+        return a[index] < b[index];
+    } else{
+        return a < b;
+    }
+}
+
+vector<string> solution(vector<string> strings, int n) {
+    vector<string> answer;
+    index = n;
+    
+    sort(strings.begin(), strings.end(), strangeOrder);
+    
+    return strings;
+}
